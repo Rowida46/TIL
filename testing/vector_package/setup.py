f1fd301c9e2fd7c393b681351932f0ef28dfbf5a
Rowida <rowidanagah462000@gmail.com>
2022-02-19 04:42:26 +0200

    here we go

--- testing/vector_package/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='vector_operations',
-      version='0.1',
-      description='handle operations on vectors',
-      packages=['vector_operations'],
-      zip_safe=False)
