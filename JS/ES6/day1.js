ad859d4cc747a4fa9d2b645906de8855ce150420
Rowida <rowidanagah462000@gmail.com>
2022-12-30 09:19:09 +0200

    till es6 day1

--- JS/ES6/day1.js
+++ JS/ES6/day1.js
@@ -4,33 +4,52 @@
 
 /**
  * to handel val scope
- * var : is a vitual scope -> it's accessable or seen even if a var val is defined insidea block  
+ * var : is a vitual scope -> it's accessable or seen even if a var val is defined insidea block 
+ * var are not a block scope
+ * let is a block scope
  */
 /**
- * let and var are both a block scope
+ * let and var are both a function scope
  */
+
 for(var i=0; i<=4; i++){
     console.log(i)
 }
 
 console.log(i) /**i is defiend inside a block scope however it's seen here */
 
+function test(){
+    for(var ifunc=0; ifunc<=4; ifunc++){
+    console.log(ifunc)
+}}
+//console.log(ifunc) // raise error cuz var is a function block
+
+for(let ii =0; ii<=4; ii++){
+    console.log(ii)
+}
+
+// console.log(ii) cerror cuz let is a block scope
 //console.log(p)
 
-var x = 10;
-console.log(typeof x)
+/** see the  */
+var name;
+var x;
+console.log(typeof x) /**not a preserved key, so it's undefined */
+console.log(typeof name) /**works as window.name  ->aka preserved key*/
 
+let nameL;
+console.log(typeof nameL)
 
 function test(n){
     if (n){
         let msg = "hello, " + n
     }
-    //console.log(msg)
+    //console.log(msg) /** cuz let is a block scope */
 } 
 
 test("nana")
 
-testArrowFunction = (x,y) => x+y; /**single line */
+testArrowFunction = (x,y) => x+y; /** arrow function -> single line */
 
 testArrowFunction2 = (X,Y) =>{ /**MULTIPLE LINES */
     console.log(X+Y)
@@ -42,6 +61,10 @@ console.log(testArrowFunction(8,7))
 /** this -> to refer to the current obj, workds as a caller.  */
 
 console.log(this, "this window obbj")
+function test(){
+    console.log(this);
+}
+test();
 
 var obj ={
     "id" : 1,
@@ -50,9 +73,23 @@ var obj ={
         console.log(this)
     }
 }
-
-
 obj.print();
+
+var obj2 = {
+    objName:"menna",
+    objAge:25,
+    print:function(){
+            //this==obj
+          // var self = this;
+       setTimeout(()=>{
+            //this==window
+            //
+            console.log("--------------------",this.objName,this.objAge, this);
+        },2000);
+    }
+}
+
+obj2.print();
 
 /** destructring in objs
  * 
@@ -73,9 +110,12 @@ summ(9,9,0)
 
 
 arr = [1,2,3]
-a,,c = arr;
+const [a,,c] = arr; /**cnt be without const */
+console.log(a,c)
 
-
+var z = 60;
+var [x,y,cc] = [z,200]; /** cc is  undefined here */
+console.log(x,y,cc)
 /**
  * using rest, split 
  */
@@ -84,3 +124,16 @@ a,,c = arr;
  * every
  * sum
  */
+
+function add(a,...x){
+    console.log(a);
+    console.log(x);
+    console.log("arguments", arguments);
+
+}
+
+add(30,20);
+
+add(50,60,200);
+
+add(60,30,90,80);
